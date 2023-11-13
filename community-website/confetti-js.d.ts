c4b45566dac31533cdef44d167eb9962c7aff493
SIGEIV <b.monk03@gmail.com>
2023-11-13 08:40:10 -0500

    Update confetti-js.d.ts

--- community-website/confetti-js.d.ts
+++ community-website/confetti-js.d.ts
@@ -1,2 +1,2 @@
-// Fix for 'Could not find a declaration file for confetti-js module' - https://bobbyhadz.com/blog/typescript-could-not-find-a-declaration-file-for-module
+// Fix for 'Could not find a declarated file for confetti-js module' - https://bobbyhadz.com/blog/typescript-could-not-find-a-declaration-file-for-module
 declare module 'confetti-js';
